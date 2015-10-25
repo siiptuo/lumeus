aa5fe754d00943e02b9644ab67b6adb6d6c2548e
Tuomas Siipola <siiptuo@kapsi.fi>
2015-10-25 18:30:35 +0200

    Replace hard coded path with first controller found

--- main.c
+++ main.c
@@ -4,7 +4,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+#include <dirent.h>
+#include <limits.h>
 
 int read_integer_from_file(const char *filename)
 {
@@ -74,13 +77,41 @@ int parse_input(const char *input, int current_brightness, int max_brightness)
 
 int main(int argc, char *argv[])
 {
-    // TODO: don't hard code the path
-    int brightness     = read_integer_from_file("/sys/class/backlight/intel_backlight/brightness");
-    int max_brightness = read_integer_from_file("/sys/class/backlight/intel_backlight/max_brightness");
+    char controller[NAME_MAX] = { '\0' };
+
+    // TODO: allow specifying wanted controller
+    DIR *dp = opendir("/sys/class/backlight");
+    if (!dp) {
+        fprintf(stderr, "Failed to open directory '/sys/class/backlight'\n");
+        exit(EXIT_FAILURE);
+    }
+    struct dirent *entry;
+    while ((entry = readdir(dp)) != NULL) {
+        if (entry->d_name[0] == '.') {
+            continue;
+        }
+        memcpy(controller, entry->d_name, NAME_MAX);
+        break;
+    }
+    closedir(dp);
+
+    if (controller[0] == '\0') {
+        fprintf(stderr, "No controller found\n");
+        exit(EXIT_FAILURE);
+    }
+
+    char brightness_path[PATH_MAX];
+    snprintf(brightness_path, PATH_MAX, "/sys/class/backlight/%s/brightness", controller);
+    int brightness = read_integer_from_file(brightness_path);
+
+    char max_brightness_path[PATH_MAX];
+    snprintf(max_brightness_path, PATH_MAX, "/sys/class/backlight/%s/max_brightness", controller);
+    int max_brightness = read_integer_from_file(max_brightness_path);
+
     if (argc == 1) {
         printf("%d%\n", brightness * 100 / max_brightness);
     } else if (argc == 2) {
-        write_integer_to_file("/sys/class/backlight/intel_backlight/brightness", parse_input(argv[1], brightness, max_brightness));
+        write_integer_to_file(brightness_path, parse_input(argv[1], brightness, max_brightness));
     } else {
         return EXIT_FAILURE;
     }
