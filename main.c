eb9a61ab936988aefe7b381d6d46ab56af24981c
Tuomas Siipola <siiptuo@kapsi.fi>
2015-10-25 14:20:02 +0200

    Parse set brightness option input

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int read_integer_from_file(const char *filename)
 {
@@ -18,11 +19,42 @@ int read_integer_from_file(const char *filename)
     return value;
 }
 
+int clamp(int value, int min, int max) {
+    return (value < min) ? min : (value > max) ? max : value;
+}
+
+int parse_input(const char *input, int current_brightness, int max_brightness)
+{
+    char *end;
+    long value = strtol(input, &end, 10);
+
+    if (*end != '\0') {
+        if (*end == '%' && *(end + 1) == '\0') {
+            value = max_brightness * value / 100;
+        } else {
+            fprintf(stderr, "Failed to parse input '%s'\n", input);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (input[0] == '+' || input[0] == '-') {
+        return clamp(current_brightness + value, 0, max_brightness);
+    } else {
+        return clamp(value, 0, max_brightness);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     // TODO: don't hard code the path
     int brightness     = read_integer_from_file("/sys/class/backlight/intel_backlight/brightness");
     int max_brightness = read_integer_from_file("/sys/class/backlight/intel_backlight/max_brightness");
-    printf("%d%\n", brightness * 100 / max_brightness);
+    if (argc == 1) {
+        printf("%d%\n", brightness * 100 / max_brightness);
+    } else if (argc == 2) {
+        printf("%d/%d -> %s -> %d/%d\n", brightness, max_brightness, argv[1], parse_input(argv[1], brightness, max_brightness), max_brightness);
+    } else {
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
